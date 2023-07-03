6b63dbd19f6bcad7a46ab0896d049a5de5bdd414
yarpiin <yarpiin@gmail.com>
2023-07-03 22:45:01 +0100

    cupid: Introduce OFRP and set custom flags

---
+++ vendorsetup.sh
@@ -0,0 +1,34 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+export FOX_AB_DEVICE=1
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+export FOX_VERSION="White Wolf v1"
+export OF_MAINTAINER="yarpiin"
+export OF_HIDE_NOTCH=1
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/customzip/Magisk/Magisk.apk
+export OF_FORCE_PREBUILT_KERNEL=1
+export OF_USE_GREEN_LED=0
+export FOX_PATCH_VBMETA_FLAG=1
+export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+export OF_PATCH_AVB20=1
+
+
+#
