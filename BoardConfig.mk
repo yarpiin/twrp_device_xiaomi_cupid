172a6c8ac7cfddbfcaeff7bfe729359ab995f6b3
yarpiin <yarpiin@gmail.com>
2023-07-03 20:30:50 +0100

    cupid: set default brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,7 @@ TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 1024
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_NO_SCREEN_BLANK := true
